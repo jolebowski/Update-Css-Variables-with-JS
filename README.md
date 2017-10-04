47ac25676888cb4d3b2b5a992c819ede333c8bc6
jolebowski <jobogos@gmail.com>
2017-10-04 12:38:21 +0200

    ajout de tous les fichiers

---
+++ README.md
@@ -0,0 +1,1 @@
+update css variable with js
---
+++ app.js
@@ -0,0 +1,8 @@
+var inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    var suffix = this.dataset.sising || ''
+    document.documentElement.style.setProperty("--" + this.name, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
